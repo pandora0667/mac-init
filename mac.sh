8e30acfe7df10f0c63dee0c0b06be3f2e4db063f
seongwon <seongwon@edu.hanbat.ac.kr>
2020-01-15 14:37:02 +0900

    script update version 2

--- mac.sh
+++ mac.sh
@@ -2,11 +2,10 @@
 
 git -C "$(brew --repo homebrew/core)" fetch --unshallow
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-brew cask install adobe-acrobat-reader google-chrome jetbrains-toolbox typora visual-studio-code zulu microsoft-teams microsoft-remote-desktop-beta microsoft-office
+brew cask install homebrew/cask-versions/microsoft-remote-desktop-beta adobe-acrobat-reader google-chrome jetbrains-toolbox typora visual-studio-code zulu microsoft-teams microsoft-office docker
 
 brew install curl git glances go wget nload tmux-xpanes gradle zsh zsh-autosuggestions zsh-syntax-highlighting
+brew reinstall python
 
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
